fcabd9a215912dc81a238b0020e0b360623b995d
Fares <122798605+FaresEisa@users.noreply.github.com>
2025-03-25 11:17:23 +0000

    Delete scms2/DbConfig.cs

--- scms2/DbConfig.cs
+++
@@ -1,7 +0,0 @@
-namespace scms2
-{
-    public static class DbConfig
-    {
-        public const string ConnectionString = "Server=DESKTOP-ULE0NA9;Database=scmsdb;Trusted_Connection=True;";
-    }
-}

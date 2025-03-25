7d6afd5956d6b47f5acdf96e9f54c9a7c88c4627
Fares <122798605+FaresEisa@users.noreply.github.com>
2025-03-25 11:16:42 +0000

    Create DbConfig.cs
    only need to change the connectionString here

---
+++ scms2/DbConfig.cs
@@ -0,0 +1,7 @@
+namespace scms2
+{
+    public static class DbConfig
+    {
+        public const string ConnectionString = "Server=DESKTOP-ULE0NA9;Database=scmsdb;Trusted_Connection=True;";
+    }
+}
